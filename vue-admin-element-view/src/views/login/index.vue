3a823fe993e8e57455da539cc7fceb39d1057a2c
htmlcssscript <3239089064Qqq.com>
2017-11-26 23:29:03 +0800

    引入element-ui iview-ui, 页面成功渲染出出来

--- vue-admin-element-view/src/views/login/index.vue
+++ vue-admin-element-view/src/views/login/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <Button>Default</Button>
-    <Button type="primary">Primary</Button>
-    <Button type="ghost">Ghost</Button>
-    <Button type="dashed">Dashed</Button>
-    <Button type="text">Text</Button>
+    
   </div>
 </template>
 
